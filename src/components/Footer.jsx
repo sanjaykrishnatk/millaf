d716e3e2924e66aaba2f3a4b0904bbdce37f717a
SANJAY <sktk9909@gmail.com>
2024-11-15 21:25:58 +0530

    15-11-2024 v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,7 +11,33 @@ import c3 from "../assets/c3.png";
 import c4 from "../assets/c4.png";
 import c5 from "../assets/c5.png";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
 function Footer() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, message } = formData;
+
+    const whatsappNumber = "918075578701";
+    const whatsappMessage = `Name: ${name}. Email: ${email} Message: ${message}`;
+    const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+      whatsappMessage
+    )}`;
+    window.open(url, "_blank");
+  };
   return (
     <div className="footer">
       <Row className="mx-0 py-5 d-flex">
@@ -81,6 +107,14 @@ function Footer() {
           className="mx-0 p-0 d-flex flex-column footer-column"
         >
           <Row className="mx-0 d-flex align-itmes-center justify-content-center">
+            <p
+              className="m-0 footer-text p-0 fw-bold mb-2"
+              style={{ fontSize: "18px" }}
+            >
+              CERTIFICATIONS
+            </p>
+          </Row>
+          <Row className="mx-0 d-flex align-itmes-center justify-content-center">
             <img
               src={c1}
               alt="certification-logo"
@@ -116,30 +150,46 @@ function Footer() {
           </Row>
         </Col>
         <Col md={4} sm={12} className="mx-0  d-flex flex-column ">
-          <input
-            type="text"
-            className="form-control bg-transparent rounded-0 text-light mb-4"
-            placeholder="Name"
-            style={{ border: "1px solid #C0C0C0" }}
-          />
-          <input
-            type="text"
-            className="form-control bg-transparent rounded-0 text-light mb-4"
-            placeholder="Email"
-            style={{ border: "1px solid #C0C0C0" }}
-          />
-          <textarea
-            className="form-control bg-transparent rounded-0 text-light mb-4"
-            placeholder="Message"
-            style={{
-              border: "1px solid #C0C0C0",
-              color: "white",
-              height: "130px",
-            }}
-          />
-          <div className="d-flex justify-content-end">
-            <button className="footer-button mb-2">Submit</button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control bg-transparent rounded-0 text-light mb-4"
+              placeholder="Name"
+              style={{ border: "1px solid #C0C0C0" }}
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              className="form-control bg-transparent rounded-0 text-light mb-4"
+              placeholder="Email"
+              style={{ border: "1px solid #C0C0C0" }}
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <textarea
+              className="form-control bg-transparent rounded-0 text-light mb-4"
+              placeholder="Message"
+              style={{
+                border: "1px solid #C0C0C0",
+                color: "white",
+                height: "130px",
+              }}
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+            <div className="d-flex justify-content-end">
+              <button type="submit" className="footer-button mb-2">
+                Submit
+              </button>
+            </div>
+          </form>
         </Col>
       </Row>
     </div>
